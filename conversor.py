2344c02d61a18371ba8799e7e131fb5350841ddf
Natan Oliveira <natannjos@gmail.com>
2019-02-03 21:02:37 -0200

    Add comments in the function

--- conversor.py
+++ conversor.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 
-def xlsx_to_json(filepath):
-	result = pd.read_excel(io=filepath).to_json(orient='records')
+def xlsx_to_json(filepath, lines=False):
+	"""Function that converts a xlsx format file into json and creates other file with the result
+	(filepath=string, lines=bool)
+	The filepath parameter informs the path to the xlsx file to be converted 
+	The lines parameter informs if the output will be in one or many lines
+	"""
+	result = pd.read_excel(io=filepath).to_json(orient='records', lines=lines)
 	f = open('result.json', "w")
 	f.write(result)
 	f.close()
